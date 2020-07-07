abee48c391f6692b522f72b27960cf7107fd20d4
bidhanrai424@gmail.com <gitlab.com>
2020-07-07 20:14:54 +0545

    added food, quit and continue with another game option after loss

--- index.py
+++ index.py
@@ -14,18 +14,11 @@ dis_height = 600
 
 snake_speed = 15
 snake_block = 10
-x1 = 300
-y1 = 300
-
-#for changing postion of moving snake
-x1_change = 0
-y1_change = 0
 
 
 #display a screen
 dis = pygame.display.set_mode((dis_width,dis_height))
 pygame.display.set_caption('Snake game by Sampang')
-game_over = False
 
 clock = pygame.time.Clock()
 
@@ -34,45 +27,70 @@ font_style = pygame.font.SysFont(None, 50)
  
 def message(msg,color):
     mesg = font_style.render(msg, True, color)
-
-    dis.blit(mesg, [round(dis_width/2.3), round(dis_height/2)])
+    dis.blit(mesg, [round(1), round(dis_height/2)])
  
 
-foodx = round(random.randrange(0, dis_width-snake_block)/10) *10
-foody = round(random.randrange(0, dis_height-snake_block)/10) *10
+def gameLoop():
+    game_over = False
+    game_close = False
 
-while not game_over:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            game_over = True
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                x1_change = -10
-                y1_change = 0
-            if event.key == pygame.K_RIGHT:
-                x1_change = 10
-                y1_change = 0
-            if event.key == pygame.K_UP:
-                x1_change = 0
-                y1_change = -10
-            if event.key == pygame.K_DOWN:
-                x1_change = 0
-                y1_change = 10
-    if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
-        game_over = True
-    
-    x1 += x1_change
-    y1 += y1_change
-    dis.fill(black)
+    x1 = dis_width/2
+    y1 = dis_height/2
 
-    pygame.draw.rect(dis, red, [foodx,foody,10,10])
-    pygame.draw.rect(dis, blue, [x1,y1,10,10])
+    #for changing postion of moving snake
+    x1_change = 0
+    y1_change = 0
 
-    pygame.display.update()
-    clock.tick(snake_speed)
-message("You Lost", red)
-pygame.display.update()
-time.sleep(2)
+    foodx = round(random.randrange(0, dis_width-snake_block)/10) *10
+    foody = round(random.randrange(0, dis_height-snake_block)/10) *10
 
-pygame.quit()
-quit()
+    while not game_over:
+        while game_close==True:
+            dis.fill(black)
+            message("You Lost! Press Q-Quit or C-Continue again", red)
+            pygame.display.update()
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_q:
+                        game_over = True
+                        game_close = False
+                    if event.key == pygame.K_c:
+                        gameLoop()
+  
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_over = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x1_change = -10
+                    y1_change = 0
+                if event.key == pygame.K_RIGHT:
+                    x1_change = 10
+                    y1_change = 0
+                if event.key == pygame.K_UP:
+                    x1_change = 0
+                    y1_change = -10
+                if event.key == pygame.K_DOWN:
+                    x1_change = 0
+                    y1_change = 10
+        if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
+            game_close = True
+        
+        x1 += x1_change
+        y1 += y1_change
+        dis.fill(black)
+
+        pygame.draw.rect(dis, red, [foodx,foody,10,10])
+        pygame.draw.rect(dis, blue, [x1,y1,10,10])
+
+        pygame.display.update()
+
+        if x1 == foodx and y1 == foody:
+            print('Yummy!')
+
+        clock.tick(snake_speed)
+
+    pygame.quit()
+    quit()
+
+gameLoop()
